31a4801f1d04ba6c2e04f9dd20c6e0cd63456af4
amoo34 <a.moizfaisal123@gmail.com>
2024-04-02 04:01:36 +0500

    update hardcoded PORT on console to variable

--- server.js
+++ server.js
@@ -49,5 +49,5 @@ app.get("/oauth2callback", async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:3000`);
+  console.log(`Server is running on ${PORT}`);
 });
